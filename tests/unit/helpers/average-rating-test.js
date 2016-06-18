0e90fb9c2e801ffdc112c2dc8166bfbd071712fd
johndeezy <jldjld503@gmail.com>
2016-06-17 18:42:27 -0700

    added helper for star but it breaks on refresh

---
+++ tests/unit/helpers/average-rating-test.js
@@ -0,0 +1,10 @@
+import { averageRating } from 'js-code-review-4/helpers/average-rating';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | average rating');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = averageRating([42]);
+  assert.ok(result);
+});
