d16af50ec84a14313703742cde28d1a86b7b2bf7
john <jldjld503@gmail.com>
2016-06-18 13:28:26 -0700

    created service that shows list of favorite games, needs frontend reworking but project is technically complete

--- app/components/favorite-list/component.js
+++ app/components/favorite-list/component.js
@@ -2,9 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoriteGames: Ember.inject.service(),
-  actions: {
-    addToFavorites(item) {
-      this.get('favoriteGames').add(item);
-    },
-  }
 });
--- app/components/non-sense/component.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-    genreName: Ember.computed('game.gameName', 'game.gameGenre', function() {
-        return this.get('game.gameName') + '|' + this.get('game.gameGenre');
-    }),
-});
